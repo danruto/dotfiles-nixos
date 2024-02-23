4fe9f08f959aa26eb359236feda1762acc055bd2
danruto <danny@pixelbru.sh>
2024-02-23 14:33:56 +1100

    first cut nix packaging myvim

--- user/apps/terminal/myvim.nix
+++ user/apps/terminal/myvim.nix
@@ -1,17 +1,10 @@
-{ config, lib, pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   programs.neovim = {
     enable = true;
     # package = pkgs.unstable.neovim;
     extraPackages = with pkgs; [
-      # LazyVim
-      # lua-language-server
-      # stylua
-      # gopls
-      # rust-analyzer
-      # nodePackages.typescript-language-server
-
       # Telescope
       ripgrep
       nixpkgs-fmt
@@ -33,56 +26,108 @@
           };
         };
         plugins = with pkgs.vimPlugins; [
-          cmp-buffer
-          cmp-nvim-lsp
-          cmp-path
-          cmp_luasnip
-          dressing-nvim
-          flash-nvim
-          friendly-snippets
-          gitsigns-nvim
-          indent-blankline-nvim
-          neo-tree-nvim
-          noice-nvim
-          nui-nvim
-          nvim-cmp
-          nvim-lspconfig
-          nvim-notify
-          nvim-spectre
-          nvim-treesitter
-          nvim-treesitter-context
-          nvim-treesitter-textobjects
           nvim-ts-autotag
-          nvim-ts-context-commentstring
-          nvim-web-devicons
-          plenary-nvim
-          telescope-fzf-native-nvim
-          telescope-nvim
-          todo-comments-nvim
-          tokyonight-nvim
-          trouble-nvim
-          vim-illuminate
-          vim-startuptime
-          { name = "LuaSnip"; path = luasnip; }
-          { name = "catppuccin"; path = catppuccin-nvim; }
-          { name = "mini.ai"; path = mini-nvim; }
-          { name = "mini.bracketed"; path = mini-nvim; }
-          { name = "mini.bufremove"; path = mini-nvim; }
-          { name = "mini.comment"; path = mini-nvim; }
-          { name = "mini.indentscope"; path = mini-nvim; }
-          { name = "mini.pairs"; path = mini-nvim; }
-          { name = "mini.surround"; path = mini-nvim; }
-          { name = "mini.trailspace"; path = mini-nvim; }
-          crates-nvim
-          nvim-navic
-          rust-tools-nvim
-          typescript-nvim
-          tailwindcss-colorizer-nvim
-          neovim-ayu
-          papercolor-theme
           editorconfig-nvim
           vim-closetag
           tagalong-vim
+          nvim-treesitter
+          nvim-treesitter-context
+          nvim-treesitter-textobjects
+          nvim-treesitter-textsubjects
+          nvim-treesitter-endwise
+          flash-nvim
+          # { name = "mini.ai"; path = mini-nvim; }
+          { name = "mini.bracketed"; path = mini-nvim; }
+          { name = "mini.bufremove"; path = mini-nvim; }
+          { name = "mini.comment"; path = mini-nvim; }
+          { name = "mini.cursorword"; path = mini-nvim; }
+          { name = "mini.doc"; path = mini-nvim; }
+          { name = "mini.hues"; path = mini-nvim; }
+          { name = "mini.indentscope"; path = mini-nvim; }
+          { name = "mini.pairs"; path = mini-nvim; }
+          { name = "mini.splitjoin"; path = mini-nvim; }
+          { name = "mini.surround"; path = mini-nvim; }
+          # { name = "mini.statusbar"; path = mini-nvim; }
+          { name = "mini.trailspace"; path = mini-nvim; }
+          plenary-nvim
+          diffview-nvim
+          gitsigns-nvim
+          neogit
+          nvim-lspconfig
+          nvim-navbuddy
+          nvim-navic
+          nui-nvim
+          copilot-lua
+          crates-nvim
+          nvim-cmp
+          cmp-buffer
+          cmp-nvim-lsp
+          lspkind-nvim
+          cmp-nvim-lsp-signature-help
+          cmp-path
+          cmp-nvim-lua
+          cmp_luasnip
+          cmp-under-comparator
+          cmp-cmdline
+          cmp-nvim-lsp-document-symbol
+          copilot-cmp
+          none-ls-nvim
+          typescript-tools-nvim
+          rust-tools-nvim
+          nvim-dap
+          nvim-dap-ui
+          hover-nvim
+          lsp-inlayhints-nvim
+          fidget-nvim
+          barbecue-nvim
+          # lsp-lens
+          friendly-snippets
+          { name = "LuaSnip"; path = luasnip; }
+          impatient-nvim
+          nvim-web-devicons
+          nvim-notify
+          dressing-nvim
+          Shade-nvim
+          # expressline
+          #icon-picker.nvim
+          nvim-colorizer-lua
+          true-zen-nvim
+          winshift-nvim
+          #floate
+          trouble-nvim
+          telescope-nvim
+          neo-tree-nvim
+          # github-nvim-theme
+          neovim-ayu
+          # bloop
+          papercolor-theme
+          # snowy
+          # ohlucy
+          # moonbow
+          # danger
+          # nvim-colo
+          tokyonight-nvim
+          # nightly
+          nordic-nvim
+
+
+
+
+
+
+
+          # Lazy plugins we can check out later
+          # indent-blankline-nvim
+          # noice-nvim
+          # nvim-spectre
+          # nvim-ts-context-commentstring
+          # telescope-fzf-native-nvim
+          # todo-comments-nvim
+          # vim-illuminate
+          # vim-startuptime
+          # { name = "catppuccin"; path = catppuccin-nvim; }
+          # typescript-nvim
+          # tailwindcss-colorizer-nvim
         ];
         mkEntryFromDrv = drv:
           if lib.isDerivation drv then
@@ -92,6 +137,8 @@
         lazyPath = pkgs.linkFarm "lazy-plugins" (builtins.map mkEntryFromDrv plugins);
       in
       ''
+
+        require("dantoki")
         require("lazy").setup({
           defaults = {
             lazy = true,
@@ -107,8 +154,42 @@
             { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = {} } },
             { "williamboman/mason-lspconfig.nvim", enabled = false },
             { "williamboman/mason.nvim", enabled = false },
+            { import = "dantoki/plugins" },
+          },
+          performance = {
+            rtp = {
+              disabled_plugins = {
+                "2html_plugin",
+                "getscript",
+                "getscriptPlugin",
+                "gzip",
+                "logipat",
+                "netrw",
+                "netrwPlugin",
+                "netrwSettings",
+                "netrwFileHandlers",
+                "matchit",
+                "tar",
+                "tarPlugin",
+                "rrhelper",
+                "spellfile_plugin",
+                "vimball",
+                "vimballPlugin",
+                "zip",
+                "zipPlugin",
+                "tutor",
+                "rplugin",
+                "syntax",
+                "synmenu",
+                "optwin",
+                "compiler",
+                "bugreport",
+                "ftplugin",
+              },
+            },
           },
         })
+
       '';
   };
 
@@ -149,6 +230,8 @@
     in
     "${parsers}/parser";
 
-  # Normal LazyVim config here, see https://github.com/LazyVim/starter/tree/main/lua
-  xdg.configFile."nvim/lua".source = ./configs/nvim;
+  xdg.configFile."nvim/lua".source = ./configs/nvim/lua;
 }
+
+# TODO: Port to nix:
+# barbecue, bloop.nvim, editorconfig-vim, express_line.nvim, floate.nvim, github-nvim-theme, icon-picker.nvim, lsp-lens.nvim, lsp_lines.nvim, lspkind.nvim, lush.nvim, mini.nvim, moonbow.nvim, nightly.nvim, nvim-colo, nvim-tresitter-refactor, oh-lucy, snowy0vim
