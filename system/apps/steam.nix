bac070c630484345607f030199e995bf08ebaf5b
Danny <danny@pixelbru.sh>
2026-03-08 12:27:18 +1100

    version bump

--- system/apps/steam.nix
+++ system/apps/steam.nix
@@ -1,7 +1,7 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  hardware.opengl.driSupport32Bit = true;
+  hardware.graphics.enable32Bit = true;
   programs.steam.enable = true;
   environment.systemPackages = [ pkgs.steam ];
 }
